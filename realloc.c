c68fd89869fb68511a96879a8d717a50715ebf77
sonny97-gif <charitywachira18@gmail.com>
2023-05-26 06:51:50 -0700

    updated

---
+++ realloc.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the pointer to the memory area
+ * @b: the byte to fill *s with
+ * @n: the amount of bytes to be filled
+ * Return: (s) a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+
